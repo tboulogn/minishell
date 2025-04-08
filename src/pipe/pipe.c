b50dc57ae5c834e0717e40411c85fea2902958bb
Rei Yada <ryada@c3r1p5.42perpignan.fr>
2025-04-08 15:39:11 +0200

    all pipe functions are less than 25 lines

--- src/pipe/pipe.c
+++ src/pipe/pipe.c
@@ -6,7 +6,7 @@
 /*   By: ryada <ryada@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 14:31:13 by ryada             #+#    #+#             */
-/*   Updated: 2025/04/07 16:51:43 by ryada            ###   ########.fr       */
+/*   Updated: 2025/04/08 15:32:33 by ryada            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,24 @@ void	init_pipe_struct(t_pipe *pro, int cmd_count)
 	pro->next[1] = -1;
 }
 
-void	close_all_pipe(t_pipe *pro)
+void close_parent_pipes(t_pipe *pro)
+{
+	if (pro->prev[0] != -1)
+		close(pro->prev[0]);
+	if (pro->prev[1] != -1)
+		close(pro->prev[1]);
+}
+
+void close_child_pipes(t_pipe *pro)
 {
 	if (pro->prev[0] != -1)
 		close(pro->prev[0]);
 	if (pro->prev[1] != -1)
 		close(pro->prev[1]);
 	if (pro->next[0] != -1)
-		close(pro->prev[0]);
+		close(pro->next[0]);
 	if (pro->next[1] != -1)
-		close(pro->prev[1]);
+		close(pro->next[1]);
 }
 
 void	update_pipe(int *prev, int *next)
@@ -90,6 +98,8 @@ void	first_child(t_args *args, t_cmd *cmd, t_env *env_list, t_pipe pro)
 	close(pro.next[0]);
 	if (cmd->infile)
 		read_files(cmd, fd, 1);
+	else
+		dup2(pro.prev[0], STDIN_FILENO);
 	redirect_and_close(pro.next[1], STDOUT_FILENO);
 	ft_exec(args, &env_list);
 }
@@ -107,7 +117,7 @@ void	middle_child(t_args *args, t_cmd *cmd, t_env *env_list, t_pipe pro)
 		read_files(cmd, fd_out, 3);
 	dup2(pro.prev[0], STDIN_FILENO);
 	dup2(pro.next[1], STDOUT_FILENO);
-	close_all_pipe(&pro);
+	close_child_pipes(&pro);
 	ft_exec(args, &env_list);
 }
 
@@ -178,26 +188,6 @@ void	wait_children(t_args *args, t_pipe *pro)
 	g_signal = last_exit;
 }
 
-void	single_builtin(t_args *args, t_env **env_list)
-{
-	int fd_in;
-	int fd_out;
-
-	ft_exec(args, env_list);
-	// if (args->infile)
-	// {
-	// 	fd_in = open(args->infile, O_RDONLY);
-	// 	if (fd_in < 0)
-	// 		perror(args->infile);
-	// }
-	// if (args->outfile)
-	// {
-	// 	fd_out = open(args->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	// 	if (fd_out < 0)
-	// 		perror(args->outfile);
-	// }
-}
-
 void	set_here_doc_in(t_args *args, t_cmd *current)
 {
 	int j;
@@ -212,54 +202,68 @@ void	set_here_doc_in(t_args *args, t_cmd *current)
 	}
 }
 
-void	pipex(t_args *args, t_env **env_list)
+int	no_files(t_cmd *cmd)
 {
-    t_pipe pro;
-    t_cmd *current;
-    t_args temp;
-    int i;
-	int j;
-	int status;
-	int hd_fd;
+	if (!cmd->infile && !cmd->outfile && !cmd->append_outfile)
+		return (1);
+	return (0);
+}
 
-	//if it is only one builtin, we do it manualy, we dont pass it to pipes.
-    if (args->cmd_count == 1 && !ft_check_buildin(args))
-    {
-		single_builtin(args, env_list);
-        return ;
-    }
-	//setting the input for here_doc
-	j = 0;
+void	handle_pipe_and_fork(t_pipe	*pro, t_cmd *current, int i)
+{
+	if (current->next && pipe(pro->next) == -1)
+	{
+		perror("pipe");
+		free(pro->pid);
+		exit(EXIT_FAILURE);
+	}
+	pro->pid[i] = fork();
+	if (pro->pid[i] == -1)
+	{
+		perror("fork");
+		free(pro->pid);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	init_temp_args(t_args *temp, t_cmd *current, int cmd_count)
+{
+	ft_memset(temp, 0, sizeof(t_args));
+	temp->cmd = current;
+	temp->cmd_count = cmd_count;
+}
+
+void	close_and_update(t_pipe *pro)
+{
+	close_parent_pipes(pro);
+	update_pipe(pro->prev, pro->next);
+}
+
+void	pipex(t_args *args, t_env	**env_list)
+{
+	t_pipe	pro;
+	t_cmd	*current;
+	t_args	temp;
+	int 	i;
+
 	current = args->cmd;
 	if (args->limiter)
 		set_here_doc_in(args, current);
-	//pipes
-    i = 0;
-    init_pipe_struct(&pro, args->cmd_count);
-    while (current)
-    {
-        ft_memset(&temp, 0, sizeof(t_args));
-        temp.cmd = current;
-        temp.cmd_count = args->cmd_count;
-        if (current->next && pipe(pro.next) == -1)
-        {
-            perror("pipe");//need to free
-            exit(EXIT_FAILURE);
-        }
-		pro.pid[i] = fork();
-        if (pro.pid[i] == -1)
-        {
-            perror("fork");//need to free
-            exit(EXIT_FAILURE);
-        }
-        if (pro.pid[i] == 0)
-            child_process(&temp, current, *env_list, pro, i);
-        close_all_pipe(&pro);
-        update_pipe(pro.prev, pro.next);
-        i++;
-        current = current->next;
-    }
-    close_all_pipe(&pro);
+	if (args->cmd_count == 1 && !ft_check_buildin(args) && no_files(current))
+		return (ft_exec(args, env_list));
+	init_pipe_struct(&pro, args->cmd_count);
+	i = 0;
+	while(current)
+	{
+		init_temp_args(&temp, current, args->cmd_count);
+		handle_pipe_and_fork(&pro, current, i);
+		if (pro.pid[i] == 0)
+			child_process(&temp, current, *env_list, pro, i);
+		close_and_update(&pro);
+		current = current->next;
+		i++;
+	}
+	close_parent_pipes(&pro);
 	wait_children(args, &pro);
-    free(pro.pid);
+	free(pro.pid);
 }
